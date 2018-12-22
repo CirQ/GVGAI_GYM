7201a2ea5bee03abcd805dd02791552486d37cff
WANG_Sinan <11849180@mail.sustc.edu.cn>
2018-12-23 03:32:06 +0800

    reformat and comments

--- Agent.py
+++ Agent.py
@@ -6,6 +6,7 @@ import random
 
 import numpy as np
 
+KeyType = str
 
 class Sprite(IntEnum):
     Background = 154
@@ -22,18 +23,20 @@ class Action(IntEnum):  # assign names to action id
     RIGHT = 3
 
     @classmethod
-    def random_choice(cls): # randomly choose an action
+    def random_action(cls): # randomly choose an action
         all_choices = [
             cls.USE,
             cls.LEFT,
             cls.RIGHT ]
         return random.choice(all_choices)
 
+    @classmethod
+    def has_action(cls, v):
+        return (v in cls) or any((v==e.value) for e in cls)
 
 
 class QTableError(Exception):   # base error about QTable
     pass
-
 
 class QTable(object):       # a Q table
     def __init__(self):
@@ -41,16 +44,16 @@ class QTable(object):       # a Q table
 
     def __check_key(self, key):         # check if is int or is the form (hash, action)
                                         # return True if is int, False if is tuple
-        if isinstance(key, int):
+        if isinstance(key, KeyType):
             if key not in self.table:   # add a new row if no such key
                 self.table[key] = np.array([0.0, 0.0, 0.0, 0.0])
             return True
         else:
             if len(key) != 2:
                 raise QTableError('invalid key: '+str(key))
-            if not isinstance(key[0], int):
+            if not isinstance(key[0], KeyType):
                 raise QTableError('invalid state hash: '+str(key[0]))
-            if key[1] not in Action:
+            if not Action.has_action(key[1]):
                 raise QTableError('invalid action: '+str(key[1]))
             if key[0] not in self.table:    # add a new row if no such key
                 self.table[key[0]] = np.array([0.0, 0.0, 0.0, 0.0])
@@ -78,31 +81,34 @@ class QTable(object):       # a Q table
 
 
 class Agent(object):
-    def __init__(self, alpha=0.1, gamma=0.9, Qtable=None):
+    def __init__(self, epsilon, alpha=0.1, gamma=0.9, reuse=False):
         self.name = 'Agent11849180'
-        self.Qtable = self._load_Qtable(Qtable)
+        self.model_name = 'str_cubic_exp'
+        self.Qtable = self._load_Qtable(reuse)
+        # for game map
         self.gamegrid = (9, 10)
         self.factor = 10
+        # for state hash
         self.rolling_a = 69576527601013534054379470050410071764306367636630478633119489403438659847711
         self.rolling_N = 102755400274853561541650296157828008998572747603972956759050025850260627630159
+        # for bellman equation
         self.alpha = alpha
         self.gamma = gamma
-        self.epsilon = 1.0
+        self.epsilon = epsilon
+        self.total_score = 0
 
-    def _load_Qtable(self, qtable):
-        if qtable:
-            self.model_name = qtable
-            with open(qtable, 'rb') as r:
+    def _load_Qtable(self, reuse):      # load an existing model
+        if reuse:
+            with open(self.model_name+'.pickle', 'rb') as r:
                 return pickle.load(r)
         else:
-            self.model_name = 'q-table.pickle'
             return QTable()
 
-    def _dump_Qtable(self):
-        with open(self.model_name, 'wb') as w:
+    def _dump_Qtable(self):             # dump the current model
+        with open(self.model_name+'.pickle', 'wb') as w:
             pickle.dump(self.Qtable, w)
 
-    def _convolution(self, gamemap):
+    def _convolution(self, gamemap):    # perform a convolution on the game map
         # kernal = np.ones(shape=(self.factor, self.factor, 3), dtype=np.uint16)
         conv = np.zeros(shape=self.gamegrid, dtype=np.uint16)
         height, width = self.gamegrid
@@ -111,22 +117,24 @@ class Agent(object):
             conv[i,j] = np.sum(area)    # equivalent to all kernal elements are 1s
         return conv
 
-    def _rolling_hash(self, gamemap):
+    def _rolling_hash(self, gamemap):   # rolling hash algorithm
         H = 0
         for c in gamemap.reshape(-1):
             H += (self.rolling_a*H + c) % self.rolling_N
         return H
 
-    def hashing(self, gamemap):
-        return self._rolling_hash(self._convolution(gamemap))
+    def hashing(self, gamemap):         # for state compress
+        # return self._rolling_hash(self._convolution(gamemap))
+        return str(self._convolution(gamemap))
 
-    def get_next_action(self, state):
+    def get_next_action(self, state):       # epsilon-greedy strategy
         if random.random() > self.epsilon:  # do exploitation
-            return self.Qtable[state].max()
+            return self.Qtable[state].argmax()
         else:                               # do exploration
-            return Action.random_choice()
+            return Action.random_action()
 
     def update_Qtable(self, state0, act, state1, reward):
+        reward **= 3
         currentQ = self.Qtable[state0, act]
         deltaQ = reward + self.gamma * self.Qtable[state1].max() - currentQ
         self.Qtable[state0, act] = currentQ + self.alpha * deltaQ
@@ -138,8 +146,10 @@ class Agent(object):
         stateObs, increScore, done, debug = env.step(action)
         new_state = self.hashing(stateObs)
         env.render()
+        self.total_score += increScore
         self.update_Qtable(this_state, action, new_state, increScore)
         if done:
+            print('Score is:', self.total_score)
             self._dump_Qtable()
         return done, debug
 
--- trainRLAgent.py
+++ trainRLAgent.py
@@ -4,14 +4,17 @@ import gym_gvgai
 import Agent as Agent
 
 env = gym_gvgai.make('gvgai-aai-lvl0-v0')
-agent = Agent.Agent()
-print('Starting ' + env.env.game + " with Level " + str(env.env.lvl))
-stateObs = env.reset()
-actions = env.env.GVGAI.actions()
 
-for t in range(2000):
-    done, debug = agent.train_act(env, actions)
+epsilon = 1.0
+MAX_EPISODE = 10000
 
-    print("gameTick", t, "winState", debug["winner"])
-    if done:
-        break
+for r in range(MAX_EPISODE):
+    epsilon *= 0.9995   # minmum is 0.006729527022146667
+    agent = Agent.Agent(epsilon, reuse=True)
+    stateObs = env.reset()
+    actions = env.env.GVGAI.actions()
+    for t in range(1000):
+        done, debug = agent.train_act(env, actions)
+        if done:
+            break
+    print("episode", r, "epsilon", epsilon, "gameTick", t, "winState", debug["winner"])
